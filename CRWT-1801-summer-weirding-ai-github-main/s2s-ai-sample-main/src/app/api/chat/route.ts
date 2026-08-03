fbb5f8ede9e7ce1910dfac268a3f0ae44dd8d16d
rachelyyj724 <rachelyyj724@gmail.com>
2026-08-03 04:40:07 -0400

    Update route.ts

--- CRWT-1801-summer-weirding-ai-github-main/s2s-ai-sample-main/src/app/api/chat/route.ts
+++ CRWT-1801-summer-weirding-ai-github-main/s2s-ai-sample-main/src/app/api/chat/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: Request) {
     const { messages } = await request.json();
 
     const completion = await openai.chat.completions.create({
-      model: 'rachelyyj724-c8f0/emotionallostandfound',
+      model: 'rachelyyj724-c8f0/Qwen3.5-9B-cbf765da',
       messages,
       stream: false,
     });
